298e6bf024c08c3a70b80d8e91dc02b056ab2dcc
miss-candy-girl <846471465@qq.com>
2021-06-29 00:40:05 +0800

    模板字符串

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  /****** case one *******/
+  const downloadBtn = ({ filename, ext }) => {
+    const download = () => {
+      import(`../public/files/${filename}.${ext}`).then(file => {
+        const linkElemet = document.createElement('a')
+        linkElemet.setAttribute('href', file.default)
+        linkElemet.setAttribute('download', `${filename}.${ext}`)
+        linkElemet.style.visibility = 'hidden'
+        document.body.appendChild(linkElemet)
+        linkElemet.click()
+        document.body.removeChild(linkElemet)
+      })
+      return false
+    }
+    download()
+  }
+  downloadBtn()
+  /****** case two *******/
+  const form = {}
+  const item = {
+    buyername: 'candy girl',
+    taxnum: 1314520,
+    address: '杭州',
+    telephone: 1314520,
+    account: 1314520,
+    phone: 1314521,
+    email: '1314520@qq.com'
+  }
+  form?.setFieldsValue({
+    buyername: item.buyername || '',
+    taxnum: item.taxnum || '',
+    address: `${item.address || ''}${item.telephone || ''}`,
+    account: item.account || '',
+    phone: item.phone || '',
+    email: item.email || ''
+  })
   return (
     <div className="App">
       <header className="App-header">
